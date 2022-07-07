03e5070df761240e4a78c0b361dc155a010afcdf
manish <4121.manish@gmail.com>
2022-07-07 11:37:34 +0300

    added form validation on front and back

--- home/forms.py
+++ home/forms.py
@@ -1,6 +1,13 @@
 from django import forms
 from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
 
+
+def check_forbidden_chars(value):
+    forbiddenChars = ['<', '>', '{', '}', '#', '^', '~', '|', '*']
+    test = (any(char in value for char in forbiddenChars)) or (value == '' or value == None) 
+    if test:
+        raise ValidationError('Invalid chars found')
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, label='Ваше Имя:',
@@ -13,4 +20,5 @@ class ContactForm(forms.Form):
     email = forms.EmailField(max_length=200, label='Email:',  widget=forms.TextInput({'placeholder': 'Email'}), 
                             validators=[RegexValidator('^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$',
                             message='Invalid Email')])
-    description = forms.CharField(max_length=3000, widget=forms.Textarea({'placeholder': 'Описание'}), label='Описание:')
+    description = forms.CharField(max_length=3000, widget=forms.Textarea({'placeholder': 'Описание'}),
+                                  validators=[check_forbidden_chars], label='Описание:')
--- home/static/js/contactform.js
+++ home/static/js/contactform.js
@@ -1,29 +1,53 @@
 
 function contact(form){
+    const forbiddenChars = ['<', '>', '{', '}', '#', '^', '~', '|', '*']
     const formData = {
         name: form.querySelector('[name=name]').value,
         email: form.querySelector('[name=email]').value,
         phone_no: form.querySelector('[name=phone_no]').value,
         description: form.querySelector('[name=description]').value
     }
+
+    let formHasEmtpy = false
+    for (let key in formData){
+        if (formData[key] == '' || formData[key] == null){
+            console.log(`a ${formData[key]}`)
+            errMsg = "Поле Обязательное"
+            formHasEmtpy = true
+            ShowContactFormFieldError(form, key, errMsg)
+        }
+    }
+
+    if (formHasEmtpy){
+        return null
+    }
+
     const csrfToken = form.querySelector('[name=csrfmiddlewaretoken]').value
     const nameReTest = /^([a-zA-Za-яё]\s?)+$/.test(formData.name) ;
     const emailReTest = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(formData.email);
     const phoneReTest = /^((8|\+7)[\- ]?)?9(\(?\d{2}\)?[\- ]?)?[\d\- ]{7,10}$/.test(formData.phone_no)
+    const descriptionReTest = !forbiddenChars.some(char => formData.description.includes(char))
 
-    if (nameReTest && emailReTest && phoneReTest){
+    if (nameReTest && emailReTest && phoneReTest && descriptionReTest){
         console.log('Test passed')
         let resp = sendContactReq(formData, csrfToken)
         console.log(resp)
     } else {
         if (!nameReTest){
-            console.log('name failed test')
+            let errMsg = 'Только Русские и Латинские букви доступны'
+            ShowContactFormFieldError(form, 'name', errMsg)
         }
         if (!emailReTest){
-            console.log('email failed test')
+            let errMsg = 'Почта неправильно указана'
+            ShowContactFormFieldError(form, 'email', errMsg)
         }
         if (!phoneReTest){
-            console.log('phone failed test')
+            let errMsg = 'Номер неправильно указана'
+            ShowContactFormFieldError(form, 'phone_no', errMsg)
+        }
+        if (!descriptionReTest){
+            let errMsg = `В тексте не разрешаются симболы ${forbiddenChars.toString().slice(1,-1)}`
+            ShowContactFormFieldError(form, 'phone_no', errMsg)
         }
     }
 }
@@ -39,3 +63,11 @@ async function sendContactReq(formData, csrf){
     }).then(res => resp=res.data)
     return resp
 }
+
+
+function ShowContactFormFieldError(form, fieldName, errorMsg){
+    let error = form.querySelector(`[name=${fieldName}]`).nextElementSibling
+            error.innerHTML = errorMsg
+            error.style.display = 'block'
+}
+
