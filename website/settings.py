19e70bfd54a253549fc010181c06320cffe4366e
4121m <4121manish@gmail.com>
2023-08-31 09:43:57 +0300

    typo fix

--- website/settings.py
+++ website/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
-from pip._internal.metadata.importlib._compat import BasePath
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
